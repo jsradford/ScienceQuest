7d3786c5fc749199e6c8e0598d6038551f3df274
Tim Crosby <tim.crosby@gmail.com>
2018-05-20 15:41:04 -0400

    NPCs in village give directions and say name

--- client/ts/entity/character/npc/npc.ts
+++ client/ts/entity/character/npc/npc.ts
@@ -4,46 +4,29 @@ import {Character} from '../character';
 
 export const NpcTalk = {
     'guard': [
-      'Hello there',
-      'We don\'t need to see your identification',
-      'You are not the player we\'re looking for',
-      'Move along, move along...'
+      'Welcome to ScienceQuest! Go to house on the hill in the center of the village to play a game.',
+      'My name is guard. I am a guard.'
     ],
 
     'king': [
       'Hi, I\'m the King',
-      'I run this place',
-      'Like a boss',
-      'I talk to people',
-      'Like a boss',
-      'I wear a crown',
-      'Like a boss',
-      'I do nothing all day',
-      'Like a boss',
-      'Now leave me alone',
-      'Like a boss'
+      'Leave this house and go South and West to the house on the hill to play a game'
     ],
 
     'villagegirl': [
-      'Welcome to ScienceQuest!',
-      'You can play games from <iframe src="http://volunteerscience.com/experiments/">here</iframe>. Eventually.'
+      'Welcome to ScienceQuest! Go South and East to the house on the hill to play a game.',
+      'You can play games from <a target="blank" href="http://volunteerscience.com/experiments/">here</a>. Eventually.',
+      'My name is Village Girl. Apparently.'
     ],
 
     'villager': [
-      'Howdy stranger. Do you like poetry?',
-      'Roses are red, violets are blue...',
-      'I like hunting rats, and so do you...',
-      'The rats are dead, now what to do?',
-      'To be honest, I have no clue.',
-      'Maybe the forest, could interest you...',
-      'or instead, cook a rat stew.'
+      'Welcome to ScienceQuest! Go South to the house on the hill to play a game.',
+      'My name is villager. I\'m a villager. How\'s that for nominative determanism?'
     ],
 
     'agent': [
-      'Do not try to bend the sword',
-      'That\'s impossible',
-      'Instead, only try to realize the truth...',
-      'There is no sword.'
+      'Welcome to ScienceQuest! Go South and West to the house on the hill to play a game.',
+      'I am agent.'
     ],
 
     'rick': [
@@ -83,30 +66,13 @@ export const NpcTalk = {
     ],
 
     'priest': [
-      'Oh, hello, my friend.',
-      'Wisdom is everything, so I\'ll share a few guidelines with you.',
-      'You are free to go wherever you like in this world',
-      'but beware of the many foes that await you.',
-      'You can find many weapons and armors by killing enemies.',
-      'The tougher the enemy, the higher the potential rewards.',
-      'You can also unlock achievements by exploring and hunting.',
-      'Click on the small cup icon to see a list of all the achievements.',
-      'Please stay a while and enjoy the many surprises of ScienceQuest.',
-      'Farewell, young friend.'
+      'Go South and East to the house on the hill to play a game.',
+      'I\'m a priest!'
     ],
 
     'sorcerer': [
-      'Ah... I had foreseen you would come to see me.',
-      'Well? How do you like my new staff?',
-      'Pretty cool, eh?',
-      'Where did I get it, you ask?',
-      'I understand. It\'s easy to get envious.',
-      'I actually crafted it myself, using my mad wizard skills.',
-      'But let me tell you one thing...',
-      'There are lots of items in this game.',
-      'Some more powerful than others.',
-      'In order to find them, exploration is key.',
-      'Good luck.'
+      'Welcome to ScienceQuest! Go East to the house on the hill to play a game.',
+      'So sayeth me the sorcerer.'
     ],
 
     'octocat': [
